09bcac375d178bf46ff88e6d01252f47eae2bb76
Ahu <dxqllp@163.com>
2025-01-23 15:28:32 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ python main.py --data_path Data/ --d_model 64 --d_inner_hid 64 --n_head 2 --n_la
 
 python main.py --data_path Data/ --d_model 64 --d_inner_hid 64 --n_head 1 --n_layers 1 --d_k 8 --d_v 32--lr 0.0005 --task 'Phsyio12000'  --test_only True
 
-python main.py --data_path Data/mimic_iv-2.2/ --d_model 32 --d_inner_hid 32 --n_head 1 --n_layers 2 --d_k 4 --d_v 8--lr 0.001 --task 'MIMIC-IV'  --test_only True
+python main.py --data_path Data/mimic-iv-2.2/ --d_model 32 --d_inner_hid 32 --n_head 1 --n_layers 2 --d_k 4 --d_v 8--lr 0.001 --task 'MIMIC-IV'  --test_only True
